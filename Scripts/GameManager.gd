f4e44b1944243cfe7ac77f46cb79ad96fc77098a
Erlatronics <erlandsson.gustav@gmail.com>
2024-04-13 21:04:06 +0200

    Added dialogue

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -4,14 +4,14 @@ var distace : int = 150000 # in meter
 var digs_per_meter : int = 10 #digs for one dm down
 var depth_per_dig : float = 0.1 # m
 var digs_to_depth_multiplier = 0
- 
+var resource = load("res://Assets/Dialogues/test.dialogue")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	State.money = 500
 	Signals.connect("equipment_bought",update_equipment)
 	Signals.connect("dig",dig_once)
 	digs_to_depth_multiplier = 1/(distace*digs_per_meter / depth_per_dig)
-	
+	start_dialog()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,3 +25,6 @@ func update_equipment(res : EquipmentResource):
 	equipment[res.title] = res
 func dig_once():
 	State.digs += 1
+	
+func start_dialog():
+	pass
