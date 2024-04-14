a3cdf8433ae9a52c0a5c0c8fc2baa751cc8b0a48
Per Lenander <per.lenander@addiva.se>
2024-04-14 10:09:20 +0200

    Added functions to update resources

--- Scripts/State.gd
+++ Scripts/State.gd
@@ -5,3 +5,15 @@ var dig_depth: float = 0.0
 var money: int = 0
 var manpower: int = 0
 var morale: float = 1.0
+
+func add_money(amount: int):
+	money += amount
+	Signals.emit_signal("money_updated", amount)
+
+func add_manpower(amount: int):
+	manpower += amount
+	Signals.emit_signal("manpower_updated", amount)
+
+func add_morale(amount: int):
+	morale += amount
+	Signals.emit_signal("morale_updated", amount)
