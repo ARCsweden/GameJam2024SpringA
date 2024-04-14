7ed3bd646be92d42fb024872a6a6c26765d21730
Per Lenander <per.lenander@addiva.se>
2024-04-14 10:28:45 +0200

    Tweaked starting resources

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,16 +1,22 @@
 extends Node
+
 var equipment : Dictionary = {}
 var distace : int = 150000 # in meter
 var digs_per_meter : int = 10 #digs for one dm down
 var depth_per_dig : float = 0.1 # m
 var digs_to_depth_multiplier = 0
 var resource = load("res://Assets/Dialogues/test.dialogue")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	State.money = 100
+	# Set up starting resources
+	State.add_money(100)
+	State.add_manpower(1)
+	State.add_morale(1)
+
 	Signals.connect("equipment_bought",update_equipment)
 	Signals.connect("dig",dig_once)
-	digs_to_depth_multiplier = 1/(distace*digs_per_meter / depth_per_dig)
+	digs_to_depth_multiplier = 1 / (distace * digs_per_meter / depth_per_dig)
 	start_dialog()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,11 +26,11 @@ func _process(delta):
 		State.digs += eq.amount * eq.dig_power * delta
 	State.dig_depth  = digs_to_depth_multiplier * State.digs
 
-		 
 func update_equipment(res : EquipmentResource):
 	equipment[res.title] = res
+
 func dig_once():
 	State.digs += 1
-	
+
 func start_dialog():
 	pass
--- Scripts/State.gd
+++ Scripts/State.gd
@@ -4,7 +4,7 @@ var digs : float = 0.0
 var dig_depth: float = 0.0
 var money: int = 0
 var manpower: int = 0
-var morale: float = 1.0
+var morale: float = 0.0
 
 func add_money(amount: int):
 	money += amount
